3523cc4608ea210b40a8f9b588e78211f8c5d11e
LimSujin0 <LimSujin0@naver.com>
2019-04-12 10:57:19 +0900

    make main

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void get_random_inarray();
+void pick_two();
+void add_two_print();
+int main(){
+	int myarray[10];
+	get_random_inarray();
+	pick_two();
+	add_two_print();
+}
